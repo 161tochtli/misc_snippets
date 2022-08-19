7de1d04888bbbb8638057e24b8436aade0b70b37
Juan Patrón <japatronvi@ciencias.unam.mx>
2022-08-18 22:16:52 -0500

    Update numerical_curiosities.py

--- numerical_curiosities.py
+++ numerical_curiosities.py
@@ -1,14 +1,16 @@
 # If you multiply any number by 9 and sum digits of the result, then sum digits again and so forth, you eventually reach 9
+
 def magic9(x, limit=100):
+    
+    s = x * 9
+    print(x, "*", 9, "=", s)
 
     i = 0
-    s = str(x*9)
-    print(x,"*",9,"=",s)
-    while True:
+    while len(str(s)) > 1 and i < limit:
         s = sum([int(digito) for digito in str(s)])
         print(s)
-        if len(str(s)) == 1 or i == limit:
-            break
-    return s
+        i += 1
 
-magic9(299_792_458)
+
+magic9(299_792_458,10)
+
